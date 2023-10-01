c3208b401020c2eeffc3ae91024774e50b7e9431
alejandragachomed <alejandra.gacho@tprs.vu.lt>
2023-10-01 17:47:18 +0200

    oct 1 18:47

--- app/src/main/java/com/example/firstlab/MainActivity.kt
+++ app/src/main/java/com/example/firstlab/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.firstlab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.view.View
+import android.widget.Toast
+import com.example.firstlab.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener { // Your code to handle the button click event goes here
+            // For example, you can display a toast message:
+            Toast.makeText(applicationContext, "Button Clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
