796972455449c02deec7ff862324c8933a4af1c4
alejandragachomed <alejandra.gacho@tprs.vu.lt>
2023-10-01 23:41:21 +0200

    oct 1 00:41

--- app/src/main/java/com/example/firstlab/MainActivity.kt
+++ app/src/main/java/com/example/firstlab/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.firstlab
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.view.View
 import android.widget.Toast
 import com.example.firstlab.R
+import android.widget.TextView
+import android.graphics.Color
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
         val button2 = findViewById<Button>(R.id.button2)
-        button2.setOnClickListener { // Your code to handle the button click event goes here
-            // For example, you can display a toast message:
-            Toast.makeText(applicationContext, "Button Clicked!", Toast.LENGTH_SHORT).show()
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "Welcome again!", Toast.LENGTH_SHORT).show()
+            textView.setTextColor(Color.BLUE)
+        }
+        button2.setOnClickListener {
+            Toast.makeText(applicationContext, "This is Android Studio!", Toast.LENGTH_SHORT).show()
+            textView2.setTextColor(Color.MAGENTA)
         }
     }
 
